ea3375df84827e9e625f8d59ecb033d6b4a25eea
fraoustin <fraoustin@gmail.com>
2022-06-11 10:44:23 +0200

    <fix> add support Redmine 5

--- lib/default_context_due_application_hooks.rb
+++ lib/default_context_due_application_hooks.rb
@@ -1,4 +1,4 @@
 require 'redmine'
-class DefaultMenuDueApplicationHooks < Redmine::Hook::ViewListener
+class DefaultContextDueApplicationHooks < Redmine::Hook::ViewListener
   render_on :view_issues_context_menu_start, :partial => 'context_due/update_context'
 end
